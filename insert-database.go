15eede85fd9cc2d0e08a2306decfefeb73666b68
litoninot <litoninot12345@gmail.com>
2022-01-28 12:55:01 +0000

    change insert database

--- insert-database.go
+++ insert-database.go
@@ -4,22 +4,18 @@ import (
 	"database/sql"
 	"fmt"
 	"time"
-	"sync"
-	"os"
+	"sync"		
 	"strconv"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var wg sync.WaitGroup
 
+
 func insertData(thread int){
 	fmt.Println(thread);
 
-	database_my := os.Getenv("database_my")	
-
-	fmt.Println(database_my)
-
-	db, err := sql.Open("mysql", "root:root@tcp(" + database_my + ":3306)/crm_mediatama")
+	db, err := sql.Open("mysql", "root:root@tcp(172.22.0.2:3306)/crm_mediatama")
 	
 	if err != nil {
 		panic(err)
@@ -34,16 +30,17 @@ func insertData(thread int){
 	sqlStr := "INSERT INTO visits(date, school_id, user_id, discription) VALUES "
 	vals := []interface{}{}
 
-	for i := 0;i < 10;i++{
-	    sqlStr += "(?, ?, ?),"
-	    vals = append(vals, "2020-01-01", 1, 1,"des-" + strconv.Itoa(i))
+	for i := 0;i < 10;i++ {
+	    sqlStr += "(?, ?, ?, ?),"
+	    vals = append(vals, "2020-01-01", "1", "1","des-golang-" + strconv.Itoa(i))
 	}
 
 	// trim the last ,
 	sqlStr = sqlStr[0:len(sqlStr)-1]
-
+	
 	// prepare the statement
-	stmt, errStmt := db.Prepare(sqlStr)
+	stmt,errStmt := db.Prepare(sqlStr)
+
 
 	if(errStmt != nil){
 		panic(errStmt)
